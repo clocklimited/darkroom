e9c02570f141747a2719c4114dbf5a91db322623
Tom Gallacher <tomgallacher23@gmail.com>
2013-03-04 17:15:57 +0000

    Adding test to check multiple images being created simultaneously

--- test/resize.test.js
+++ test/resize.test.js
@@ -4,6 +4,7 @@ var Resize = require('../lib/resize')
   , join = require('path').join
   , tmp = join(__dirname, 'fixtures', 'temp')
   , resize
+  , _ = require('lodash')
 
 describe('ResizeStream', function() {
 
@@ -50,22 +51,61 @@ describe('ResizeStream', function() {
     })
   })
 
-  it('should return an image with the dimensions of 77x12', function (done) {
+  this.timeout(60000)
+  it('should return an image with the dimensions multiple times', function (done) {
+    var ready = 0
+      , amount = 20
+      , files = []
+
+    for (var i = 0; i <= amount; i++) {
+      var resize2 = new Resize()
+      var filepath = join(tmp, i + 'xmultiple.png')
+        , readStream = fs.createReadStream(join(__dirname, 'fixtures', 'bill.png'))
+        , writeStream = fs.createWriteStream(filepath)
+
+      files.push(filepath)
+
+      readStream.pipe(resize2).pipe(writeStream
+      , { width: 300
+        , height: 300
+        }
+      )
+
+      writeStream.on('close', function() {
+        fs.readFile(filepath, function (err, data) {
+          if (err) throw err
+          data.length.should.equal(100614)
+          ready++
+        })
+      })
+    }
+    var poll = setInterval(function() {
+      if (ready > amount) {
+        _.each(files, function(file) {
+          fs.unlinkSync(file)
+        })
+        done()
+        clearInterval(poll)
+      }
+    }, 10)
+  })
+
+  it('should return an image with the dimensions of 100x200', function (done) {
     resize.chunks.should.have.lengthOf(0)
-    var filepath = join(tmp, '77x12.png')
+    var filepath = join(tmp, '100x200.png')
       , readStream = fs.createReadStream(join(__dirname, 'fixtures', 'bill.png'))
       , writeStream = fs.createWriteStream(filepath)
 
     readStream.pipe(resize).pipe(writeStream
-    , { width: 77
-      , height: 12
+    , { width: 100
+      , height: 200
       }
     )
 
     writeStream.on('close', function() {
       fs.readFile(filepath, function (err, data) {
         if (err) throw err
-        data.length.should.equal(2305)
+        data.length.should.equal(26258)
         done()
       })
     })
