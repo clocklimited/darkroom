efaafc3bb2c7931714ad960933720c48ae969a0e
Luke Wilde <luke.wilde@clock.co.uk>
2013-03-19 17:07:29 +0000

    Adding skeletal crop logic.

--- test/crop.test.js
+++ test/crop.test.js
@@ -1,41 +1,41 @@
-var Resize = require('../lib/resize')
+var Crop = require('../lib/crop')
   , streamToTest = require('./stream.fixture.js')
   , fs = require('fs')
   , join = require('path').join
   , tmp = join(__dirname, 'fixtures', 'temp')
-  , resize
+  , crop
 
-describe('ResizeStream', function() {
+describe('CropStream', function() {
 
   beforeEach(function() {
-    resize = new Resize()
+    crop = new Crop()
   })
 
   describe('Inherit from DarkroomStream', function () {
     it('should have a pipe method', function () {
-      resize.should.be.a('object').and.have.property('pipe')
+      crop.should.be.a('object').and.have.property('pipe')
     })
 
     it('should have a write method', function () {
-      resize.should.be.a('object').and.have.property('write')
+      crop.should.be.a('object').and.have.property('write')
     })
 
-    it('should shouldn\'t have a pause method', function () {
-      resize.should.be.a('object').and.not.have.property('pause')
+    it('shouldn\'t have a pause method', function () {
+      crop.should.be.a('object').and.not.have.property('pause')
     })
   })
 
   it('should read in an image using streams', function () {
-    resize.chunks.should.have.lengthOf(0)
-    streamToTest(resize)
+    crop.chunks.should.have.lengthOf(0)
+    streamToTest(crop)
   })
 
   it('should return an image with a known length', function (done) {
-    resize.chunks.should.have.lengthOf(0)
+    crop.chunks.should.have.lengthOf(0)
     var readStream = fs.createReadStream(join(__dirname, 'fixtures', 'bill.png'))
       , writeStream = fs.createWriteStream(join(tmp, 'little-bill.png'))
 
-    readStream.pipe(resize).pipe(writeStream
+    readStream.pipe(crop).pipe(writeStream
     , { width: 200
       , height: 200
       }
@@ -43,7 +43,9 @@ describe('ResizeStream', function() {
 
     writeStream.on('close', function() {
       fs.readFile(join(tmp, 'little-bill.png'), function (err, data) {
-        if (err) throw err
+        if (err) {
+          throw err
+        }
         data.length.should.equal(51372)
         done()
       })
@@ -51,12 +53,12 @@ describe('ResizeStream', function() {
   })
 
   it('should return an image with the dimensions of 100x200', function (done) {
-    resize.chunks.should.have.lengthOf(0)
+    crop.chunks.should.have.lengthOf(0)
     var filepath = join(tmp, '100x200.png')
       , readStream = fs.createReadStream(join(__dirname, 'fixtures', 'bill.png'))
       , writeStream = fs.createWriteStream(filepath)
 
-    readStream.pipe(resize).pipe(writeStream
+    readStream.pipe(crop).pipe(writeStream
     , { width: 100
       , height: 200
       }
@@ -64,28 +66,9 @@ describe('ResizeStream', function() {
 
     writeStream.on('close', function() {
       fs.readFile(filepath, function (err, data) {
-        if (err) throw err
-        data.length.should.equal(26258)
-        done()
-      })
-    })
-  })
-
-  it('should return an image with the dimensions of 100x200', function (done) {
-    resize.chunks.should.have.lengthOf(0)
-    var filepath = join(tmp, '100x200.png')
-      , readStream = fs.createReadStream(join(__dirname, 'fixtures', 'bill.png'))
-      , writeStream = fs.createWriteStream(filepath)
-
-    readStream.pipe(resize).pipe(writeStream
-    , { width: 100
-      , height: 200
-      }
-    )
-
-    writeStream.on('close', function() {
-      fs.readFile(filepath, function (err, data) {
-        if (err) throw err
+        if (err) {
+          throw err
+        }
         data.length.should.equal(26258)
         done()
       })
