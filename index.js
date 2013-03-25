18e341e39e87f8b7bb4874edb6a4f6a6a15d1bdb
Tom Gallacher <tomgallacher23@gmail.com>
2013-03-25 16:02:58 +0000

    Fixing cario version error messages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if (require('./package').cairo !== require('canvas').cairoVersion) {
   throw new Error('Please ensure your cairo version is set to '
-    + require('canvas').cairoVersion
-    + ' current version is at: ' + require('./package').cairo)
+    + require('./package').cairo
+    + ' current version is at: ' + require('canvas').cairoVersion)
   return false
 }
 
