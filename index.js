772209c06e866e40489616db3304de7f520771fc
Tom Gallacher <tomgallacher23@gmail.com>
2013-03-27 19:09:44 +0000

    Removing cairo check on index.

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-if (require('./package').cairo !== require('canvas').cairoVersion) {
-  throw new Error('Please ensure your cairo version is set to '
-    + require('./package').cairo
-    + ' current version is at: ' + require('canvas').cairoVersion)
-  return false
-}
-
 module.exports = require('./lib/darkroom')
