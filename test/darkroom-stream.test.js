97dfc03950c07967b0457025cd83f38f0fbc1160
Jack Burgess <jackburgess124@gmail.com>
2021-04-19 18:52:08 +0100

    refactor(darkroom-stream): Use class interface

--- test/darkroom-stream.test.js
+++ test/darkroom-stream.test.js
@@ -1,8 +1,9 @@
-var darkroomStream = require('../lib/darkroom-stream')(),
+var DarkroomStream = require('../lib/darkroom-stream'),
   streamToTest = require('./stream.fixture.js')
 
 describe('DarkroomStream', function () {
   it('should act as a stream', function () {
+    var darkroomStream = new DarkroomStream()
     streamToTest(darkroomStream)
   })
 })
