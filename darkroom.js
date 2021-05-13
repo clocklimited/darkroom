adf9643810f7def131bc6e7d68b0bd4b0427a44e
rymate1234 <ryan.murthick@gmail.com>
2021-05-13 21:38:12 +0100

    feat: Add a new Blur method

--- darkroom.js
+++ darkroom.js
@@ -3,5 +3,6 @@ module.exports = {
   Info: require('./lib/info'),
   Crop: require('./lib/crop'),
   Watermark: require('./lib/watermark'),
-  Circle: require('./lib/circle')
+  Circle: require('./lib/circle'),
+  Blur: require('./lib/blur')
 }
